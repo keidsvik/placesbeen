b418c0ff329283e47cbfe3d277af5859f292edae
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-15 11:50:47 -0700

    add in forms to html document and an area for the results to eventually display
    Co-authored by: Lizzie Van Hook <evanhook360@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,28 @@
 
 // Business logic for Travel Book
 function TravelBook() {
-  this.place = [];
+  this.places = [];
+  this.currentId = 0;
 }
 
+TravelBook.prototype.addPlace = function(place) {
+  place.id = this.assignId();
+  this.places.push(place);
+}
+
+TravelBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+TravelBook.prototype.findPlace = function() {
+  for (let i=0; i< this.places.length; i++) {
+    if(this.places[i].id == id) {
+      return this.places[i];
+    }
+  };
+  return false;
+}
 // Business logic for Destinations
 
 function Place (placeName, landmarks, timeOfYear, notes) {
